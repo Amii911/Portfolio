84e59f7a0648fb0ef949c3d5b6761fe0b75ab0f1
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-09-29 06:57:25 -0400

    checking resume

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1>Full Stack Software Engineer </h1>
+      <h3>New York, NY</h3>
+      <img/> add picture here
+      <br/>
+      <p>Hello I'm Aminat. Welcome!</p>
+      <br/>
+      <>need to add a ref to resume</>
+    </div>
+  )
+}
+
+export default About
+
+/*
+TO DO:
+add picture in folder
+add resume in folder
+fix styling
+*/
