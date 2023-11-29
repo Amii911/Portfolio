2a755ddcf65d838f5541f641abbd4a9fced79c8d
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-28 20:53:55 -0500

    adding about me

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,15 +1,11 @@
 import React from "react"
 import './About.css'
-// import headshot from '../../../assets/headshot.jpg'
-// import resume from '../../../assets/Sofia_Katzman_Resume.pdf'
+import headshot from '../../Assets/Aminat_Adebayo_Headshot.JPG'
+import resume from '../../Assets/Aminat_Adebayo_Resume.pdf'
 // import Contact from '../../Layout/Contact/Contact'
 import { Link } from "react-router-dom"
 
 function About(){
-
-    const redirectToDesign = () => {
-        window.location.href = 'https://sofia-katzman-design-04eb286d966b.herokuapp.com/';
-      };
 
     return(
         <div className="about">
@@ -18,13 +14,12 @@ function About(){
                     <img src={headshot} alt="Headshot" className="circle-image" />
                 </div>
                 <div className="about-blurb">
-                    <h2>Full Stack Web Developer</h2>
+                    <h2>Full Stack Software Engineer</h2>
                     <h3>New York, NY </h3>
-                    <p>Hello! I'm Sofia - I build things.</p>
+                    <p>Hello! I'm Aminat - I build things and enjoy creating user friendly applications.</p>
                     <p>Whether it's full stack applications or diverse creative projects, my background in operations, logistics, and design enables me to craft well-rounded solutions that consider every aspect of the development process.</p>  
                     <br/><br/><br/>
                     <a href={resume} download className="resume">Download My Resume</a><br/><br/><br/>
-                    <button onClick={redirectToDesign} className="resume">View My Design Portfolio</button>
                 </div>
             </div>
         </div>
