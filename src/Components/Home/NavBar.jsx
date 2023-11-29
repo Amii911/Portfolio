c353abbd21fa7dddd99648c9c3157dc51ded38f1
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-28 22:38:25 -0500

    update nav

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -1,37 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css'
 import ContactMe from './ContactMe';
 import Home from './Home';
 import Projects from '../Projects/Projects';
 
 function NavBar() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
+  useEffect(() => {
+    if (isNavOpen) {
+      document.body.style.overflow = "hidden" // Disable scrolling
+    } else {
+      document.body.style.overflow = "auto" // Enable scrolling
+    }
+
+    return () => {
+      document.body.style.overflow = "auto" // Ensure scrolling is re-enabled when component unmounts
+    }
+  }, [isNavOpen])
 
   const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
-  };
+    setIsNavOpen(!isNavOpen)
+  }
 
+  const hideNav = () => {
+    setIsNavOpen(false)
+  }
 
   return (
-    <nav className={`header ${darkMode ? 'dark-mode' : ''}`}>
-      <Link to="/" className="logo" onClick={toggleNav}>
-        Aminat Adebayo
-      </Link>
-      <div className="nav-links">
-        <Link to="/" element={<Home />}>
-          home
-        </Link>
-        <Link to="/projects" element={<Projects />}>
-          projects
-        </Link>
-        <Link to="/contactme" element={<ContactMe />} >
-          contact me
-        </Link>
+    <nav className="header">
+      <div className="logo">Aminat Adebayo</div>
+      <div className={`nav-links ${isNavOpen ? 'active' : ''}`}>
+        <Link onClick={hideNav} to="/">home</Link>
+        <Link onClick={hideNav} to="/projects">projects</Link>
+        <Link onClick={hideNav} to="/contactme">contact me</Link>
+      </div>
+      <div className="burger" onClick={toggleNav}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
-  );
+  )
 }
-
 export default NavBar;
+
 
