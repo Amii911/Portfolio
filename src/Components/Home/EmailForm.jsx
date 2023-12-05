2ecc983bbc7b202a80dbf8acbdbd97e08d9d1b75
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-05 17:23:40 -0500

    email form

--- src/Components/Home/EmailForm.jsx
+++ src/Components/Home/EmailForm.jsx
@@ -13,7 +13,7 @@ function EmailForm() {
 
     const serviceId = 'service_8w6ew6n'
     const templateId = 'template_xk7udic'
-    const userId = 'KCn-rDWXBti4PH3V2NExx'
+    const userId = 'QJqotmzAwpBZYS8Fe'
 
     emailjs
       .send(serviceId, templateId, {
